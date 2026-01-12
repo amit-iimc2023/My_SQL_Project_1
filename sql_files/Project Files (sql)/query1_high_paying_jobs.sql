6327a461f0f2014f74b9fa4d52c25cebcfed88e3
amit-iimc2023 <amitnikam9211@gmail.com>
2026-01-12 21:00:01 +0530

    Commit query1

---
+++ sql_files/Project Files (sql)/query1_high_paying_jobs.sql
@@ -0,0 +1,22 @@
+/*query1
+Problem 1: What are the top paying jobs for following criteria
+- Job Role: Data Analyst
+- Work From Home job postings
+- Remove Null Values
+- Include company name as well (from a different table)
+*/
+
+SELECT
+    job_id,
+    job_title_short AS Job_Role,
+    job_location,
+    salary_year_avg AS Average_Salary,
+    company_dim.name AS Company_Name
+FROM job_postings_fact
+LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE
+    job_title_short = 'Data Analyst' AND
+    job_work_from_home = TRUE AND
+    salary_year_avg IS NOT NULL
+ORDER BY salary_year_avg DESC
+LIMIT 10
